c1e9197f691cc1d0a6a271a01fdc70d57864b1a8
spadaritucks <thiago.spadari02@gmail.com>
2025-04-04 14:30:04 -0300

    Adicionando a coluna status

--- back-end/database/migrations/2025_04_04_144127_create_table_recomendations.php
+++ back-end/database/migrations/2025_04_04_144127_create_table_recomendations.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string("youtube_url");
+            $table->string("status");
             $table->timestamps();
         });
     }
