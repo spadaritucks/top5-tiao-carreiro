490c2048570ff33f34f37f2ec67b4a67e38b8959
spadaritucks <thiago.spadari02@gmail.com>
2025-04-04 13:11:03 -0300

    Criação dos Endpoints do Usuario

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix("/users")->group(function () {
+    Route::get('/', [UsersController::class, "getAllUsers"]);
+    Route::get('/{id}', [UsersController::class, "getUserById"]);
+    Route::post('/', [UsersController::class, "createUser"]);
+});
