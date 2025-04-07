eab185b22812c87e0fea9c1781379d687a584da5
spadaritucks <thiago.spadari02@gmail.com>
2025-04-07 19:14:45 -0300

    Organização das Rotas Web

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/login/page.tsx'
+import RegisterUser from './pages/register/page.tsx'
+import AdminPanel from './pages/admin/page.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/admin" element={<AdminPanel />}  />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
